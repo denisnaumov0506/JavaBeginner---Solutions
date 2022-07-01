36e276a497a35014a5553c87c00b7bbff6e3e11a
uniWuerDenis <94964844+uniWuerDenis@users.noreply.github.com>
2022-07-01 11:05:03 +0200

    Add files via upload

---
+++ Chapter 1/MoonEarthWeight.java
@@ -0,0 +1,15 @@
+public class MoonEarthWeight {
+
+	public static void main(String[] args) {
+		double weightEarth, weightMoon;
+		
+		weightEarth = 75; // I weigh 75 Kg on Earth
+		weightMoon = weightEarth*0.17;
+		
+		System.out.println("Your weight on earth is "
+			+ weightEarth
+			+ " Kg, but on the moon you would weigh "
+			+ weightMoon + " Kg!"
+			);
+	}
+}
